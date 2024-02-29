490aa171d6db3d02e8d4717c247dbabe525c9b20
unknown <lalit@omnipayments.com>
2024-02-29 11:41:26 +0530

    Declaring and initializing variables in python

---
+++ Declaring and initializing variables in python.py
@@ -0,0 +1,45 @@
+# Declaring and initializing variables in python
+a = 10      # single value for single variable
+b=10.23
+c="LalitVerma"
+d=True
+print(a)
+print(b)
+print(c)
+print(d)
+
+# single value for multiple variables
+a = b = c = d = 10
+print(a)
+print(b)
+print(c)
+print(d)
+
+a = b = c = d = "Lalit Verma"
+print(a)
+print(b)
+print(c)
+print(d)
+
+a = b = c = d = 10.45
+print(a)
+print(b)
+print(c)
+print(d)
+
+a = b = c = d = True
+print(a)
+print(b)
+print(c)
+print(d)
+
+# multiple values for multiple variables
+
+a,b,c,d = 10,"Lalit Verma",False,10.34
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+
